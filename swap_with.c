e44faea6d6df7dc36b79081e90c77366ef0c6d8b
Jayasurya R D <92865629+jayasuryard31@users.noreply.github.com>
2021-10-21 21:18:18 +0530

    Create swap_with.c

---
+++ swap_with.c
@@ -0,0 +1,14 @@
+/*swaping two intergers with using third variable*/
+
+#include <stdio.h>
+int main()
+{
+    int a,b,t;
+    printf("enter the values of a and b\n");
+    scanf("%d%d",&a,&b);
+    printf("before swaping\na=%d\nb=%d\n",a,b);
+    t=a;
+    a=b;
+    b=t;
+    printf("after swaping\na=%d\nb=%d\n",a,b);
+}
