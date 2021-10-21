cc7c72fd16dbf1a3dbebacec8bb8b15bfd1ab128
Jayasurya R D <92865629+jayasuryard31@users.noreply.github.com>
2021-10-21 21:49:08 +0530

    Create compare.c

---
+++ compare.c
@@ -0,0 +1,13 @@
+/*greater in two numbers using conditional operator*/
+
+#include<stdio.h>
+int main()
+{
+    int a,b,big;
+    printf("enter the value of a and b\n");
+    scanf("%d%d",&a,&b);
+    (a>b)?(big=a):(big=b);
+    printf("then %d is big\n",big);
+  
+    return 0;
+}
